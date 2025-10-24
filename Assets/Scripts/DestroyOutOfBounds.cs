b21467d1bafabfe41bcdd241dac2475a9cc7e869
Ruksana <ruksana.b@indivillage.in>
2025-10-24 13:29:47 +0530

    Declare a variable animalPrefabs with an array

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    
     }
 }
