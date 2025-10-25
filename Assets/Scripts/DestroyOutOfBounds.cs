38f8d06bda6556941c28c2c21a87000348abce58
Ruksana <ruksana.b@indivillage.in>
2025-10-25 11:21:51 +0530

    Trigger a “Game Over” message

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,12 +15,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if an object goes past the players view in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.log("Game Over!");
             Destroy(gameObject);
         }
     
